bbdbc56de3c5de609dc535df2f644272ca66907f
Gleb Gorelov <gorelov@haulmont.com>
2015-04-24 14:59:05 +0400

    Task screens

---
+++ modules/gui/src/com/haulmont/ts/gui/entities/task/TaskEdit.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2015 com.haulmont.ts.gui.entities.task
+ */
+package com.haulmont.ts.gui.entities.task;
+
+import com.haulmont.cuba.gui.components.AbstractEditor;
+import com.haulmont.ts.entity.Task;
+
+/**
+ * @author gorelov
+ */
+public class TaskEdit extends AbstractEditor<Task> {
+}
