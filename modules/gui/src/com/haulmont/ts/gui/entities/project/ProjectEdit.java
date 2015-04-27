9c212bebdbee629c823e82a0b281a805d3d2c05c
Gleb Gorelov <gorelov@haulmont.com>
2015-04-27 14:55:18 +0400

    ProjectEdit fieldGroup fields logic

--- modules/gui/src/com/haulmont/ts/gui/entities/project/ProjectEdit.java
+++ modules/gui/src/com/haulmont/ts/gui/entities/project/ProjectEdit.java
@@ -6,14 +6,20 @@ package com.haulmont.ts.gui.entities.project;
 import com.haulmont.cuba.gui.DialogParams;
 import com.haulmont.cuba.gui.WindowManager;
 import com.haulmont.cuba.gui.components.*;
+import com.haulmont.cuba.gui.data.CollectionDatasource;
 import com.haulmont.cuba.gui.data.Datasource;
 import com.haulmont.cuba.gui.data.ValueListener;
 import com.haulmont.cuba.gui.xml.layout.ComponentsFactory;
+import com.haulmont.ts.entity.Client;
 import com.haulmont.ts.entity.Project;
+import com.haulmont.ts.entity.ProjectStatus;
+import com.haulmont.ts.service.ProjectsService;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 /**
  * @author gorelov
@@ -23,11 +29,17 @@ public class ProjectEdit extends AbstractEditor<Project> {
     private FieldGroup fieldGroup;
     @Inject
     private ComponentsFactory componentsFactory;
+    @Inject
+    private CollectionDatasource<Project, UUID> projectsDs;
+    @Inject
+    private ProjectsService projectsService;
 
     @Named("fieldGroup.parent")
     private LookupPickerField parentField;
     @Named("fieldGroup.client")
     private LookupPickerField clientField;
+
+    private List<Project> childrenProjects;
 
     @Override
     public void init(final Map<String, Object> params) {
@@ -44,7 +56,21 @@ public class ProjectEdit extends AbstractEditor<Project> {
                 clientField.setEnabled(value == null);
                 if (value != null) {
                     Project parent = (Project) value;
-                    clientField.setValue(parent.getClient());
+                    if (!parent.getClient().equals(getItem().getClient()))
+                        clientField.setValue(parent.getClient());
+                } else if (prevValue != null) {
+                    clientField.setValue(null);
+                }
+            }
+        });
+
+        clientField.addListener(new ValueListener() {
+            @Override
+            public void valueChanged(Object source, String property, Object prevValue, Object value) {
+                if (childrenProjects != null && value != null) {
+                    Client client = (Client) value;
+                    for (Project child : childrenProjects)
+                        projectsService.setClient(child, client);
                 }
             }
         });
@@ -60,6 +86,12 @@ public class ProjectEdit extends AbstractEditor<Project> {
         });
     }
 
+    @Override
+    protected void postInit() {
+        removeSelfBranchFromDs();
+        getItem().setStatus(ProjectStatus.OPEN);
+    }
+
     private PickerField.LookupAction createLookupAction(PickerField pickerField) {
         PickerField.LookupAction lookupAction = new PickerField.LookupAction(pickerField);
         lookupAction.setLookupScreenOpenType(WindowManager.OpenType.DIALOG);
@@ -69,4 +101,26 @@ public class ProjectEdit extends AbstractEditor<Project> {
                 .setResizable(true));
         return lookupAction;
     }
+
+    private void removeSelfBranchFromDs() {
+        Project project = getItem();
+        projectsDs.excludeItem(project);
+        childrenProjects = projectsService.getChildren(project);
+        for (Project child : childrenProjects)
+            projectsDs.excludeItem(child);
+    }
+
+//    private List<Project> getChildrenProjects(Project parent) {
+//        if (projectsDs.getItems().size() > 0) {
+//            List<Project> children = new ArrayList<>();
+//            for (Project child : projectsDs.getItems()) {
+//                if (parent.equals(child.getParent())){
+//                    children.add(child);
+//                    children.addAll(getChildrenProjects(child));
+//                }
+//            }
+//            return children;
+//        }
+//        return Collections.emptyList();
+//    }
 }
