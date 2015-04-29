6fa85a518ba24141252cc53ee87a9ecbb31b4c10
Gleb Gorelov <gorelov@haulmont.com>
2015-04-29 14:57:43 +0400

    Task screens improvements

--- modules/gui/src/com/haulmont/timesheets/gui/task/TaskEdit.java
+++ modules/gui/src/com/haulmont/timesheets/gui/task/TaskEdit.java
@@ -3,18 +3,22 @@
  */
 package com.haulmont.timesheets.gui.task;
 
+import com.haulmont.bali.util.ParamsMap;
 import com.haulmont.cuba.core.entity.Entity;
 import com.haulmont.cuba.gui.components.*;
+import com.haulmont.cuba.gui.components.actions.AddAction;
 import com.haulmont.cuba.gui.components.actions.RemoveAction;
+import com.haulmont.cuba.gui.data.CollectionDatasource;
+import com.haulmont.cuba.gui.data.Datasource;
+import com.haulmont.cuba.gui.data.impl.DsListenerAdapter;
 import com.haulmont.cuba.gui.xml.layout.ComponentsFactory;
+import com.haulmont.timesheets.entity.ProjectParticipant;
 import com.haulmont.timesheets.entity.Task;
 import com.haulmont.timesheets.entity.TaskStatus;
 
 import javax.inject.Inject;
 import javax.inject.Named;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * @author gorelov
@@ -25,12 +29,32 @@ public class TaskEdit extends AbstractEditor<Task> {
     protected Table participantsTable;
     @Inject
     protected ComponentsFactory componentsFactory;
+    @Inject
+    protected FieldGroup fieldGroup;
+    @Inject
+    protected Datasource<Task> taskDs;
+    @Inject
+    protected CollectionDatasource<ProjectParticipant, UUID> participantsDs;
 
     @Named("fieldGroup.project")
     protected PickerField projectField;
+    @Named("participantsTable.add")
+    protected AddAction participantsTableAdd;
 
     @Override
     public void init(Map<String, Object> params) {
+
+        fieldGroup.addCustomField("description", new FieldGroup.CustomFieldGenerator() {
+            @Override
+            public Component generateField(Datasource datasource, String propertyId) {
+                ResizableTextArea textArea = componentsFactory.createComponent(ResizableTextArea.NAME);
+                textArea.setDatasource(datasource, propertyId);
+                textArea.setHeight("100px");
+                textArea.setResizable(true);
+                return textArea;
+            }
+        });
+
         String removeColumnName = "remove";
         participantsTable.addGeneratedColumn(removeColumnName, new Table.ColumnGenerator() {
             @Override
@@ -43,15 +67,34 @@ public class TaskEdit extends AbstractEditor<Task> {
         });
         participantsTable.setColumnCaption(removeColumnName, "");
         participantsTable.setColumnWidth(removeColumnName, 35);
+
+        taskDs.addListener(new DsListenerAdapter<Task>() {
+            @Override
+            public void valueChanged(Task source, String property, Object prevValue, Object value) {
+                if ("project".equals(property)) {
+                    updateParticipantsTableAddAction();
+                    if (value == null || !value.equals(prevValue)) {
+                        Collection<ProjectParticipant> participants = participantsDs.getItems();
+                        for (ProjectParticipant participant : participants) {
+                            participantsDs.removeItem(participant);
+                        }
+                    }
+                }
+            }
+        });
     }
 
     @Override
     protected void postInit() {
         super.postInit();
         getItem().setStatus(TaskStatus.ACTIVE);
-        if (getItem().getProject() != null) {
-            projectField.setEnabled(false);
-        }
+        projectField.setEnabled(getItem().getProject() == null);
+        updateParticipantsTableAddAction();
+    }
+
+    protected void updateParticipantsTableAddAction() {
+        participantsTableAdd.setWindowParams(ParamsMap.of("project", getItem().getProject(), "multiselect", true));
+        participantsTableAdd.setEnabled(getItem().getProject() != null);
     }
 
     protected class ParticipantRemoveAction extends RemoveAction {
