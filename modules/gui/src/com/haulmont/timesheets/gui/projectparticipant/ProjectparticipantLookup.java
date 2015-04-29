ddb58610367a5266b93aa98c790ca111c138dd0f
Gleb Gorelov <gorelov@haulmont.com>
2015-04-29 14:28:38 +0400

    Lookup screens

---
+++ modules/gui/src/com/haulmont/timesheets/gui/projectparticipant/ProjectparticipantLookup.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2015 com.haulmont.timesheets.gui.projectparticipant
+ */
+package com.haulmont.timesheets.gui.projectparticipant;
+
+import com.haulmont.cuba.gui.components.AbstractLookup;
+import com.haulmont.cuba.gui.components.Table;
+import com.haulmont.cuba.gui.data.CollectionDatasource;
+import com.haulmont.timesheets.entity.Project;
+import com.haulmont.timesheets.entity.ProjectParticipant;
+import org.apache.commons.lang.BooleanUtils;
+
+import javax.inject.Inject;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+
+/**
+ * @author gorelov
+ */
+public class ProjectparticipantLookup extends AbstractLookup {
+
+    @Inject
+    protected Table projectParticipantsTable;
+    @Inject
+    protected CollectionDatasource<ProjectParticipant, UUID> projectParticipantsDs;
+
+    @Override
+    public void init(Map<String, Object> params) {
+        if (BooleanUtils.isTrue((Boolean) params.get("multiselect"))) {
+            projectParticipantsTable.setMultiSelect(true);
+        }
+
+        Project project = (Project) params.get("project");
+        if (project != null) {
+            Set<ProjectParticipant> participants = project.getParticipants();
+            projectParticipantsDs.clear();
+            for (ProjectParticipant participant : participants) {
+                projectParticipantsDs.includeItem(participant);
+            }
+        }
+    }
+}
