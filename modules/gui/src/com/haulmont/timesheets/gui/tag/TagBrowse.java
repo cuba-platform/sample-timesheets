ed9209b25bf8af2bd6aef1c61ec3fd71dafc1e87
Gleb Gorelov <gorelov@haulmont.com>
2015-05-05 16:48:42 +0400

    Filter tags ability to create Tags and TagTypes

--- modules/gui/src/com/haulmont/timesheets/gui/tag/TagBrowse.java
+++ modules/gui/src/com/haulmont/timesheets/gui/tag/TagBrowse.java
@@ -5,8 +5,15 @@ package com.haulmont.timesheets.gui.tag;
 
 import com.haulmont.cuba.gui.components.AbstractLookup;
 
+import java.util.Map;
+
 /**
  * @author gorelov
  */
 public class TagBrowse extends AbstractLookup {
+    @Override
+    public void init(Map<String, Object> params) {
+        getDialogParams().setWidth(800);
+        getDialogParams().setHeight(500);
+    }
 }
--- modules/gui/src/com/haulmont/timesheets/gui/tagtype/TagTypeBrowse.java
+++ modules/gui/src/com/haulmont/timesheets/gui/tagtype/TagTypeBrowse.java
@@ -5,8 +5,15 @@ package com.haulmont.timesheets.gui.tagtype;
 
 import com.haulmont.cuba.gui.components.AbstractLookup;
 
+import java.util.Map;
+
 /**
  * @author gorelov
  */
 public class TagTypeBrowse extends AbstractLookup {
+    @Override
+    public void init(Map<String, Object> params) {
+        getDialogParams().setWidth(800);
+        getDialogParams().setHeight(500);
+    }
 }
