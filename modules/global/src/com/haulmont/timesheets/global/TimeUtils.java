4cb174f92fa8a9dc0193f108a9f52a3b46ecc09c
Gleb Gorelov <gorelov@haulmont.com>
2015-05-19 19:03:59 +0400

    Update time parsing rules

--- modules/global/src/com/haulmont/timesheets/global/TimeUtils.java
+++ modules/global/src/com/haulmont/timesheets/global/TimeUtils.java
@@ -9,6 +9,8 @@ import org.apache.commons.lang.time.DateUtils;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author gorelov
@@ -33,10 +35,26 @@ public class TimeUtils {
     }
 
     protected static int findHours(String time) {
-        return 0;
+        return findTimeValue(time, "h|hr|hour");
     }
 
     protected static int findMinutes(String time) {
+        return findTimeValue(time, "m|min|minutes");
+    }
+
+    protected static int findTimeValue(String time, String units) {
+        String regex = "\\d+\\s*(" + units + ")";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(time);
+        if (matcher.find()) {
+            String value = matcher.group();
+            regex = "\\d+";
+            pattern = Pattern.compile(regex);
+            matcher = pattern.matcher(value);
+            if (matcher.find()) {
+                return Integer.valueOf(matcher.group());
+            }
+        }
         return 0;
     }
 
